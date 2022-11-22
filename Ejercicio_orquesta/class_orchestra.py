116caac46643361f9d51a0fba0d255f8a18fd5f9
jjramiro <j.jimenezramiro98@gmail.com>
2022-11-22 19:00:54 +0100

    ejercicio de la orquesta terminado

---
+++ Ejercicio_orquesta/class_orchestra.py
@@ -0,0 +1,126 @@
+from abc import ABC
+from random import randint
+
+from Ejercicio_orquesta.decorator import orchestra_decorator
+from Ejercicio_orquesta.orchestra_exceptions import TuneException
+from Ejercicio_orquesta.utils import utils_tuned
+from logging_general import log
+
+
+class Orchestra:
+    orchestra_list = []
+
+    def create_orchestra(self):
+        '''
+            creamos los objetos de la orquesta y los añadimos a la lista de objetos del objeto orquesta
+        '''
+        log.info("empiezan a llegar los intrumentos de la orquesta")
+        guitar = Guitar("guitarra", "cuerda", 5)
+        electric_guitar = ElectricGuitar("guitarra electrica", "cuerda", 6, 80)
+        piano = Piano("piano", "cuerda", 98)
+        drum = Drum("bateria", "percusion", 70)
+        self.orchestra_list = [guitar, electric_guitar, piano, drum]
+        log.info("los instrumentos creados son: {}, {}, {} y {}".format(guitar.name, electric_guitar.name,
+                                                                        piano.name, drum.name))
+
+    def concert(self):
+        '''
+            para dar el concierto primero debemos afinar los intrumentos y por ultimo tocarlos para ver si estan bien
+            afinados y se tocan correctamente si saltara un error
+        '''
+        for instrument in self.orchestra_list:
+            instrument.tune()
+        for tuned_instrument in self.orchestra_list:
+            try:
+                if tuned_instrument.kind == "percusion":
+                    tuned_instrument.hammer()
+                else:
+                    tuned_instrument.play()
+            except TuneException as e:
+                log.error(e)
+
+
+class Instrument(ABC):
+
+    def __init__(self, name, kind):
+        self.name = name
+        self.kind = kind
+        self._tuned = False
+
+    @property
+    def tuned(self):
+        log.info("metodo getter activado")
+        return self._tuned
+
+    @tuned.setter
+    def tuned(self, tuned_successful):
+        log.info("metodo setter activado")
+        self._tuned = tuned_successful
+
+
+class Guitar(Instrument):
+
+    def __init__(self, name, kind, string_num):
+        Instrument.__init__(self, name, kind)
+        self.string_num = string_num
+
+    @orchestra_decorator
+    def tune(self):
+        if randint(1, 10) >= 1:
+            self.tuned = True
+            log.info("el instrumento {} se ha afinado correctamente".format(self.name))
+
+    @orchestra_decorator
+    def play(self):
+        utils_tuned(self.tuned, self.name)
+
+
+class ElectricGuitar(Guitar):
+
+    def __init__(self, name, kind, string_num, power):
+        Guitar.__init__(self, name, kind, string_num)
+        self.power = power
+
+    @orchestra_decorator
+    def tune(self):
+        if randint(1, 10) >= 1:
+            self.tuned = True
+            log.info("el instrumento {} se ha afinado correctamente".format(self.name))
+
+    @orchestra_decorator
+    def play(self):
+        utils_tuned(self.tuned, self.name)
+
+
+class Piano(Instrument):
+
+    def __init__(self, name, kind, keynum):
+        Instrument.__init__(self, name, kind)
+        self.keynum = keynum
+
+    @orchestra_decorator
+    def tune(self):
+        if randint(1, 10) >= 1:
+            self.tuned = True
+            log.info("el instrumento {} se ha afinado correctamente".format(self.name))
+
+    @orchestra_decorator
+    def play(self):
+        utils_tuned(self.tuned, self.name)
+
+
+class Drum(Instrument):
+
+    def __init__(self, name, kind, size):
+        Instrument.__init__(self, name, kind)
+        self.size = size
+
+    @orchestra_decorator
+    def tune(self):
+        if randint(1, 10) >= 1:
+            self.tuned = True
+            log.info("el instrumento {} se ha afinado correctamente".format(self.name))
+
+    @orchestra_decorator
+    def hammer(self, name):
+        print("Se esta tocando el tambor {}".format(name))
